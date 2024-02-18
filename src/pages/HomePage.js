f070f986f2e60c5af9e2240816a9cf3ac2b82cc0
doberman <doberman24@yandex.ru>
2024-02-18 20:05:22 +0300

    Creating home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import { moreBtn } from "../config/AddElements";
+
+const HomePage = () => {
+    return (
+        <div className="flex w-full h-full items-center">
+            <div className="flex flex-grow items-center h-[600px]">
+                <div className="absolute h-full w-[530px] bg-[#ffa500] -z-10 skew-x-[15deg] -ml-36"></div>
+                <div className="box-content px-[4%] w-1/3">
+                    <div className="w-full aspect-square bg-my-photo bg-cover animate-ringEffect"></div>
+                </div>
+                <div className="flex flex-grow justify-center h-full">
+                    <div className="flex flex-col justify-center max-w-[600px] text-gray-600">
+                        <ul className="font-philosopher text-[3.5rem] uppercase leading-normal font-bold">
+                            <li className=" relative text-[#ffa500] leading-none pl-14 before:content-['—'] before:absolute before:left-0">Я Анатолий Самушенков.</li>
+                            <li className="pl-14 text-nowrap">WEB разработчик</li>
+                        </ul>
+                        <p className="font-jura text-[1.1rem] leading-9 text-gray-500 font-medium my-4">Я начинающий front-end разрабочик, и уже начал свой путь в мире web-разработки. Моя цель - стать профессионалом в этой области и создавать удобные и функциональные интерфейсы для web-приложений и сайтов.</p>
+                        <div className="fillMoreBtn effectFillMoreBtn relative flex border border-[#ffa500] h-14 w-60 justify-center items-center rounded-full mt-8">
+                            <button className="font-Commissioner uppercase font-bold flex flex-grow justify-center items-center h-full text-sm">узнать больше</button>
+                            <span className="flex cursor-pointer rounded-full justify-center items-center p-4 bg-[#ffa500]">{moreBtn.arrow}</span>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        philosopher: ["Philosopher", "sans-serif"],
+        jura: ["Jura", "sans-serif"],
+        Commissioner: ["Commissioner", "sans-serif"],
+        MontsAlt: ["Montserrat Alternates", "sans-serif"]
+      },
+      backgroundImage: {
+        'my-photo' : "url('/src/img/face.jpg')"
+      },
+      keyframes: {
+        ringEffect: {
+          '0%, 100%': {
+            borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%',
+            boxShadow: 'inset 0 0 0 30px rgba(255, 255, 255, .3), 15px 15px 50px rgba(0, 0, 0, .4)'
+          },
+          '50%': {
+            borderRadius: '30% 60% 70% 40% / 50% 60% 30% 60%',
+            boxShadow: 'inset 0 0 0 30px rgba(255, 255, 255, .3), -10px -5px 50px rgba(0, 0, 0, .4)'
+          }
+        }
+      },
+      animation: {
+        ringEffect: 'ringEffect 8s ease-in-out infinite'
+      },
+    },
   },
   plugins: [],
 }
